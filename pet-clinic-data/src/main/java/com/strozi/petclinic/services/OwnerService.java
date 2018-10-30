8d966626412f9429d8c1d4a1001b7a1861ab0391
strozi <michal.strozynski@o2.pl>
2018-10-30 20:24:35 +0100

    adding interfaces for service layer

---
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/OwnerService.java
@@ -0,0 +1,13 @@
+package com.strozi.petclinic.services;
+
+import com.strozi.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.strozi.petclinic.services;
+
+import com.strozi.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
