012e4ecf026cf9c8336ecff426e8c1b1846e818b
strozi <michal.strozynski@o2.pl>
2018-10-30 22:36:46 +0100

    Implementing AbstractMapService

---
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package com.strozi.petclinic.services.map;
+
+import com.strozi.petclinic.model.Owner;
+import com.strozi.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
+
+
