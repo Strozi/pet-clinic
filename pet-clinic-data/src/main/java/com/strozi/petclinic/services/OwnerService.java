46408008d6aa34ea78975f28358cf12fa3b0e545
strozi <michal.strozynski@o2.pl>
2018-10-30 22:06:43 +0100

    Refactoring services to extend common Crud Servive

--- pet-clinic-data/src/main/java/com/strozi/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/OwnerService.java
@@ -2,12 +2,7 @@ package com.strozi.petclinic.services;
 
 import com.strozi.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/strozi/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/VetService.java
@@ -2,11 +2,6 @@ package com.strozi.petclinic.services;
 
 import com.strozi.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
