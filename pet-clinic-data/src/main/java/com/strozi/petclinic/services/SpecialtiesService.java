de9fce408b6049436e79a95774249b8874cee41d
strozi <michal.strozynski@o2.pl>
2018-11-19 14:17:36 +0100

    adding specialties map impl

---
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package com.strozi.petclinic.services;
+
+import com.strozi.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/map/SpecialtiesMapService.java
@@ -0,0 +1,34 @@
+package com.strozi.petclinic.services.map;
+
+import com.strozi.petclinic.model.Speciality;
+import com.strozi.petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialtiesMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
