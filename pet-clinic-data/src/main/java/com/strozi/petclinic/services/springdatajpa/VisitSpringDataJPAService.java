cfefb3b514152c3567806a6757f5708990669ba3
strozi <michal.strozynski@o2.pl>
2018-12-06 17:48:05 +0100

    creating visit spring data jpa service

---
+++ pet-clinic-data/src/main/java/com/strozi/petclinic/services/springdatajpa/VisitSpringDataJPAService.java
@@ -0,0 +1,48 @@
+package com.strozi.petclinic.services.springdatajpa;
+
+import com.strozi.petclinic.model.Visit;
+import com.strozi.petclinic.repositories.VisitRepository;
+import com.strozi.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSpringDataJPAService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSpringDataJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.findById(aLong);
+    }
+}
